951a3055ab8b6517c289fbd2936ae3ed3a955fec
Elliot <cregliot@gmail.com>
2016-04-24 15:39:31 +0600

    Some more empty projects

---
+++ HoiTools/PersistentLayer/Core.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace PersistentLayer
+{
+    public class Core
+    {
+        public Core()
+        {
+            if (ConfigurationManager.AppSettings["Texts"] != null)
+                ;
+            if (ConfigurationManager.AppSettings["Tech"] != null)
+                ;
+        }
+    }
+    internal class TextData
+    {
+    }
+}
---
+++ HoiTools/PersistentLayerTests/CoreTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using PersistentLayer;
+using System.Configuration;
+
+namespace PersistentLayerTests
+{
+    [TestClass]
+    public class CoreTests
+    {
+        [TestMethod]
+        public void Creation()
+        {
+            Core core = new Core();
+        }
+    }
+}
