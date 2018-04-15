1d2f495f4372eb946cc84f8964e87b404f7dcbcc
Elliot <cregliot@gmail.com>
2018-04-16 02:48:21 +0700

    Tests and fixes.

--- HoiTools/PersistentLayer/Core.cs
+++ HoiTools/PersistentLayer/Core.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Configuration;
 using System.IO;
 using System.Diagnostics;
-using System.ComponentModel;
 using System.Globalization;
 
 namespace PersistentLayer
@@ -35,7 +34,7 @@ namespace PersistentLayer
                 Instance.Init(value);
             }
         }
-        static public IModels Models { get { return Instance._models; } }
+        static public IUnitTypes UnitTypes { get { return Instance._unitTypes; } }
         static public IReadOnlyCollection<string> Countries { get { return Instance._countries.Values; } }
         static internal string CurrentCountryTag { get { return Instance._countryTag; } }
         static public string CurrentCountry
@@ -53,15 +52,9 @@ namespace PersistentLayer
             if (!string.IsNullOrEmpty(root))
                 Instance.Init(root);
         }
-
-
+        
         private Core()
         {
-            foreach (UnitTypes type in Enum.GetValues(typeof(UnitTypes)))
-            {
-                _models.AddModel(type, 0, "");
-            }
-
             _countries[Constants.DefaultCountry] = "Common";
             _countryTags["Common"] = Constants.DefaultCountry;
             _countryTag = Constants.DefaultCountry;
@@ -82,8 +75,8 @@ namespace PersistentLayer
 
             var textData = _textData;
             _textData = new Dictionary<string, string>();
-            var models = _models;
-            _models = new Models();
+            var unitTypes = _unitTypes;
+            _unitTypes = new UnitTypes();
             var countries = _countries;
             _countries = new Dictionary<string, string>();
             var countryTags = _countryTags;
@@ -99,20 +92,21 @@ namespace PersistentLayer
             catch (Exception)
             {
                 _textData = textData;
-                _models = models;
+                _unitTypes = unitTypes;
                 _countries = countries;
                 _countryTags = countryTags;
 
                 throw;
             }
 
-            DataChanged?.Invoke(null, "Countries");
+            _countryTag = Constants.DefaultCountry;
+            DataChanged?.Invoke(null, "All");
         }
 
         private void ParseModelSpecifications(string root)
         {
             CultureInfo culture = CultureInfo.CreateSpecificCulture("en-US");
-            foreach (UnitTypes type in _models.Types)
+            foreach (UnitTypeName type in _unitTypes.Types)
             {
                 string filename = root + Constants.ModelSpecsPath + type + ".txt";
                 if (!File.Exists(filename))
@@ -188,7 +182,7 @@ namespace PersistentLayer
                                     }
                                     else
                                     {
-                                        _models.SetSpec(type, modelId, k, v);
+                                        _unitTypes.SetSpec(type, modelId, k, v);
                                     }
                                 }
                                 break;
@@ -236,14 +230,14 @@ namespace PersistentLayer
                         }
 
                         int type, id;
-                        if (!int.TryParse(aa[1 + shift], out type) || !Enum.IsDefined(typeof(UnitTypes), type) || !int.TryParse(aa[2 + shift], out id))
+                        if (!int.TryParse(aa[1 + shift], out type) || !Enum.IsDefined(typeof(UnitTypeName), type) || !int.TryParse(aa[2 + shift], out id))
                         {
                             ParseError(Constants.ModelsPath, s);
                             continue;
                         }
                         string country = aa[0 + shift];
 
-                        _models.AddModel((UnitTypes) type, id, a[1], country);
+                        _unitTypes.AddUnitType((UnitTypeName) type, id, a[1], country);
                     }
                 }
             }
@@ -263,13 +257,18 @@ namespace PersistentLayer
                     while ((s = sr.ReadLine()) != null)
                     {
                         if (s.StartsWith("#EOF"))
-                        {
                             break;
-                        }
+
+                        // comment or not decoded
+                        if (s.StartsWith("#") || s.Contains(";;;;;;;;;;;"))
+                            continue;
 
                         var a = s.Split(';');
                         if (a.Count() >= 2 && !string.IsNullOrWhiteSpace(a[0]) && !string.IsNullOrWhiteSpace(a[1]))
                         {
+                            if (_textData.ContainsKey(a[0]))
+                                ParseError(Constants.TextPath, "var '" + a[0] + "' overwritten (" + _textData[a[0]] + " to " + a[1] + ")");
+
                             _textData[a[0]] = a[1];
                         }
                         else
@@ -303,8 +302,19 @@ namespace PersistentLayer
                         if (a.Count() == 5 && !string.IsNullOrWhiteSpace(a[0]))
                         {
                             string country = _textData[a[0]];
-                            _countries[a[0]] = country;
-                            _countryTags[country] = a[0];
+
+                            // Skip not using countries
+                            if (country == "User Defined") continue;
+
+                            if (_countries.ContainsKey(a[0]))
+                            {
+                                Trace.WriteLine("Duplicated country: '" + a[0] + "' for country '" + country + "'");
+                            }
+                            else
+                            {
+                                _countries[a[0]] = country;
+                                _countryTags[country] = a[0];
+                            }
                         }
                         else
                         {
@@ -323,7 +333,7 @@ namespace PersistentLayer
         }
 
         private Dictionary<string, string> _textData = new Dictionary<string, string>();
-        private Models _models = new Models();
+        private UnitTypes _unitTypes = new UnitTypes();
         private Dictionary<string, string> _countries = new Dictionary<string, string>();
         private Dictionary<string, string> _countryTags = new Dictionary<string, string>();
         private string _countryTag = Constants.DefaultCountry;
--- HoiTools/PersistentLayerTests/CoreTests.cs
+++ HoiTools/PersistentLayerTests/CoreTests.cs
@@ -1,11 +1,32 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using PersistentLayer;
-using System.Configuration;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace PersistentLayerTests
 {
+    public class TraceCounter : TraceListener
+    {
+        public override void Write(string message)
+        {
+            _lines += message.Aggregate(0, (acc, chr) => { if (chr == '\n') acc++; return acc; });
+        }
+
+        public override void WriteLine(string message)
+        {
+            _lines += message.Aggregate(0, (acc, chr) => { if (chr == '\n') acc++; return acc; });
+            _lines++;
+        }
+
+        public int Lines { get => _lines; }
+        public void Reset() { _lines = 0; }
+
+        private int _lines = 0;
+    }
+
     [TestClass]
     public class CoreTests
     {
@@ -13,7 +34,7 @@ namespace PersistentLayerTests
 //        [ExpectedException(typeof(System.InvalidOperationException))]
         public void CreationNotConfigured()
         {
-            IModels models = Core.Models;
+            IUnitTypes models = Core.UnitTypes;
             Assert.IsNotNull(models);
         }
 
@@ -22,8 +43,83 @@ namespace PersistentLayerTests
         {
             Core.RootFolder = @"D:\Games\HOI\mod-CORE";
 
-            IModels models = Core.Models;
+            IUnitTypes models = Core.UnitTypes;
             Assert.IsNotNull(models);
+        }
+
+        [TestMethod]
+        public void TextDataParsed()
+        {
+            int lines = File.ReadLines(@"D:\Games\HOI\mod-CORE\config\text.csv").Count(s => { return s.StartsWith("#") || s.Contains(";;;;;;;;;;;") ? false : true; });
+            TraceCounter tc = new TraceCounter();
+
+            string root = @"D:\Games\HOI\mod-CORE";
+            PrivateObject prCore = new PrivateObject(typeof(Core));
+
+            Trace.Listeners.Add(tc);
+            prCore.Invoke("ParseTextData", root);
+
+            int textCount = ((Dictionary<string, string>) prCore.GetField("_textData")).Count;
+            Assert.AreEqual(lines, textCount + tc.Lines, "Text count ({0}) is wrong (should be {1} - {2} = {3})", textCount, lines, tc.Lines, lines - tc.Lines);
+            Assert.AreEqual(tc.Lines, 0);
+        }
+
+        [TestMethod]
+        public void ModelNamesParsed()
+        {
+            int lines = File.ReadLines(@"D:\Games\HOI\mod-CORE\config\models.csv").Count();
+            TraceCounter tc = new TraceCounter();
+
+            string root = @"D:\Games\HOI\mod-CORE";
+            PrivateObject prCore = new PrivateObject(typeof(Core));
+
+            Trace.Listeners.Add(tc);
+            prCore.Invoke("ParseModelNames", root);
+
+            int modelsCount = 0;
+            PrivateObject types = new PrivateObject(prCore.GetField("_unitTypes"));
+            foreach (var e1 in (Dictionary<UnitTypeName, UnitType>) types.GetField("_unitTypes"))
+            {
+                PrivateObject models = new PrivateObject(e1.Value);
+                foreach (var e2 in (Dictionary<int, Model>) models.GetField("_models"))
+                {
+                    PrivateObject model = new PrivateObject(e2.Value);
+                    modelsCount += ((Dictionary<string, string>) model.GetField("_namesByCountry")).Count;
+                }
+            }
+
+            Assert.AreEqual(lines - 1, modelsCount + tc.Lines, "Model count ({0}) is wrong (should be {1} - {2} = {3})", modelsCount, lines - 2, tc.Lines, lines - 2 - tc.Lines);
+        }
+
+        [TestMethod]
+        public void CountriesParsed()
+        {
+            int lines = File.ReadLines(@"D:\Games\HOI\mod-CORE\config\models.csv").Count();
+            TraceCounter tc = new TraceCounter();
+
+            string root = @"D:\Games\HOI\mod-CORE";
+            PrivateObject prCore = new PrivateObject(typeof(Core));
+            prCore.Invoke("ParseTextData", root);
+
+            Trace.Listeners.Add(tc);
+            prCore.Invoke("ParseCountries", root);
+
+            var countries = (Dictionary<string, string>) prCore.GetField("_countries");
+            int countryTags = ((Dictionary<string, string>)prCore.GetField("_countryTags")).Count;
+            Assert.AreEqual(countries.Count, countryTags, "Country count ({0}) isn't equal to tags count ({1})", countries.Count, countryTags);
+
+            prCore.Invoke("ParseModelNames", root);
+            PrivateObject types = new PrivateObject(prCore.GetField("_unitTypes"));
+            foreach (var e1 in (Dictionary<UnitTypeName, UnitType>)types.GetField("_unitTypes"))
+            {
+                PrivateObject models = new PrivateObject(e1.Value);
+                foreach (var e2 in (Dictionary<int, Model>)models.GetField("_models"))
+                {
+                    PrivateObject model = new PrivateObject(e2.Value);
+                    foreach (string countryTag in ((Dictionary<string, string>)model.GetField("_namesByCountry")).Keys)
+                        if (!countries.ContainsKey(countryTag)) Assert.Fail("Model for absent country found ({0})", countryTag);
+                }
+            }
         }
     }
 }
